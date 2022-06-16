26ca2b2b78599c9bda3b0e123d9664a523e2b45d
jaykim <jaykim@192.168.1.69>
2022-06-16 22:38:12 +0900

    개발환경 세팅

---
+++ twitterClean/src/main/java/com/jkimtoy/TwitterCleanApplication.java
@@ -0,0 +1,13 @@
+package com.jkimtoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCleanApplication.class, args);
+	}
+
+}
---
+++ twitterClean/src/test/java/com/jkimtoy/TwitterCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkimtoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
