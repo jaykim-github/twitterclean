a6078da0be26e05f7561a025aaa304a9b015a0e6
jaykim <jaykim@192.168.1.69>
2022-06-18 01:17:49 +0900

    twitter 객체 획득 후 requestToken 획득하기 인증 URL 획득 테스트 완료

--- twitterClean/src/main/java/com/jkimtoy/TwitterTest.java
+++ twitterClean/src/main/java/com/jkimtoy/TwitterTest.java
@@ -1,6 +1,7 @@
 package com.jkimtoy;
 
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
@@ -10,17 +11,25 @@ public class TwitterTest {
 	static String accessToken="";
 	static String accessSecret="";
 		
-	static Twitter twitter;	
-	RequestToken requestToken = null;
-	AccessToken finalAccessToken = null;
-	
+	static Twitter twitter;
+	static RequestToken requestToken=null;
+	AccessToken finalAccessToken=null;
+
+
 	 public static void main(String[] args){
-	
 	//twitter 객체 초기화 + consumer 인증 set
 	twitter = TwitterFactory.getSingleton();
-	twitter.setOAuthConsumer(TwitterTest.getInstance().getAPIKey(), 
-			TwitterTest.getInstance().getAPISecretKey());
 	
-	 }
+	//getInstance해도 되지만, 기존에 얻어놓은 토큰을 사용하여 넣어도 됨. getInstance할거면 메소드를 생성해야한다.
+	twitter.setOAuthConsumer(TwitterInfo.getInstance().getAPIKey(), TwitterInfo.getInstance().getAPISecretKey());
+	
+	try {
+		requestToken = twitter.getOAuthRequestToken();
+	}catch(TwitterException e) {
+		e.printStackTrace();
+	}
+	System. out.println(requestToken.getAuthorizationURL());
+	        
 
+	 }
 }
