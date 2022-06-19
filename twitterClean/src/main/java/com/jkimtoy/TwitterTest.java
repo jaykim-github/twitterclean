8c540c49378a623360943d2236cd153b434467ef
jaykim-github <jaykeem713@gmail.com>
2022-06-19 23:48:40 +0900

    twitter properties 생성 -> getOauth 토큰 문제로 생성하였으나, 관련해서 API쪽 문서를 찾아봐야 함

--- twitterClean/src/main/java/com/jkimtoy/TwitterTest.java
+++ twitterClean/src/main/java/com/jkimtoy/TwitterTest.java
@@ -1,8 +1,10 @@
 package com.jkimtoy;
 
+import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.User;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
 
@@ -13,18 +15,16 @@ public class TwitterTest {
 		
 	static Twitter twitter;
 	static RequestToken requestToken=null;
-	AccessToken finalAccessToken=null;
-
+	static AccessToken finalAccessToken=null;
 
 	 public static void main(String[] args){
 	//twitter 객체 초기화 + consumer 인증 set
 	twitter = TwitterFactory.getSingleton();
 	
 	//getInstance해도 되지만, 기존에 얻어놓은 토큰을 사용하여 넣어도 됨. getInstance할거면 메소드를 생성해야한다. 
-	/*
-	 * 깃 리베이스를 위한 테스트 용
-	 * */
-	twitter.setOAuthConsumer(TwitterInfo.getInstance().getAPIKey(), TwitterInfo.getInstance().getAPISecretKey());
+	//twitter.setOAuthConsumer(TwitterInfo.getInstance().getAPIKey(), TwitterInfo.getInstance().getAPISecretKey());
+
+	
 	
 	try {
 		requestToken = twitter.getOAuthRequestToken();
@@ -33,6 +33,19 @@ public class TwitterTest {
 	}
 	System. out.println(requestToken.getAuthorizationURL());
 	        
+	
+	twitter.setOAuthAccessToken(finalAccessToken);
+	try {
+    	//계정명 획득
+		User user = twitter.verifyCredentials();
+		System.out.println(user.getScreenName());
+		
+        //계정에 트윗 등록
+		String msg = "this is test tweet from application";
+		Status status = twitter.updateStatus(msg);		
+	}catch(Exception e) {
+		e.printStackTrace();
+}
 
 	 }
 }
