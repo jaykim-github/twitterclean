8daee550147983d4b141c1675bef77ef0e2db548
jaykim-github <jaykeem713@gmail.com>
2022-06-22 23:25:52 +0900

    2022.06.22 : properties 사용하여 apikey 숨기기 성공!!!

--- twitterClean/src/main/java/com/jkimtoy/TwitterCleanApplication.java
+++ twitterClean/src/main/java/com/jkimtoy/TwitterCleanApplication.java
@@ -1,13 +1,17 @@
 package com.jkimtoy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+
 public class TwitterCleanApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwitterCleanApplication.class, args);
+
 	}
 
 }
--- twitterClean/src/main/java/com/jkimtoy/TwitterTest.java
+++ twitterClean/src/main/java/com/jkimtoy/TwitterTest.java
@@ -2,6 +2,9 @@ package com.jkimtoy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
@@ -14,58 +17,73 @@ import twitter4j.User;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
 
-@Controller
-@PropertySource("src/main/resources/twitter.properties")
+@SpringBootApplication
 public class TwitterTest {
-	//전역변수 선언
-	static String accessToken="";
-	static String accessSecret="";
-		
+	// 전역변수 선언
+	static String accessToken = "";
+	static String accessSecret = "";
+
 	static Twitter twitter;
-	static RequestToken requestToken=null;
-	static AccessToken finalAccessToken=null;
-	
-	@Autowired
-	TwitterProperties prop;
-	
-	private static String apiKey;
-	
+	static RequestToken requestToken = null;
+	static AccessToken finalAccessToken = null;
+
 	@Value("${apiKey}")
-	private void setApiKey(String key) {
-		apiKey = key;
+	private String apiKey;
+	@Value("${apiKeySecret}")
+	private String apisecretKey;
+	
+	@Value("${accesstoken}")
+	private String accesstoken;
+	@Value("${secrettoken}")
+	private String secrettoken;
+
+	public String apikey() {
+		return this.apiKey;
 	}
 
-	 public static void main(String[] args){
-		 
-	//twitter 객체 초기화 + consumer 인증 set
-	twitter = TwitterFactory.getSingleton();
-	
-	//getInstance해도 되지만, 기존에 얻어놓은 토큰을 사용하여 넣어도 됨. getInstance할거면 메소드를 생성해야한다. 
-	//twitter.setOAuthConsumer(TwitterInfo.getInstance().getAPIKey(), TwitterInfo.getInstance().getAPISecretKey());
-	twitter.setOAuthConsumer("", "");
-	
-	try {
-		requestToken = twitter.getOAuthRequestToken();
-	}catch(TwitterException e) {
-		e.printStackTrace();
-	}
-	System. out.println(requestToken.getAuthorizationURL());
-	
-	finalAccessToken=new AccessToken("1407688173241569283-QZDLSI knGiGQEogLR1O5x6SXfNBFkl", "w5gW4F0QDAXlAwnUVJRCilmk89N4NOn33yO0ge8UFHv3O");
-	
-	twitter.setOAuthAccessToken(finalAccessToken);
-	try {
-    	//계정명 획득
-		User user = twitter.verifyCredentials();
-		System.out.println(user.getScreenName());
-		
-        //계정에 트윗 등록
-		String msg = "테스트";
-		Status status = twitter.updateStatus(msg);		
-	}catch(Exception e) {
-		e.printStackTrace();
+	public String apisecretKey() {
+		return this.apisecretKey;
 	}
 	
+	public String accesstoken() {
+		return this.accesstoken;
+	}
+	public String secrettoken() {
+		return this.secrettoken;
+	}
 
-	 }
+	public static void main(String[] args) {
+
+		try (ConfigurableApplicationContext ctx = SpringApplication.run(TwitterTest.class, args)) {
+			TwitterTest m = ctx.getBean(TwitterTest.class);		
+
+			// twitter 객체 초기화 + consumer 인증 set
+			twitter = TwitterFactory.getSingleton();
+
+			twitter.setOAuthConsumer(m.apikey(),m.apisecretKey());
+
+			try {
+				requestToken = twitter.getOAuthRequestToken();
+			} catch (TwitterException e) {
+				e.printStackTrace();
+			}
+			System.out.println(requestToken.getAuthorizationURL());
+
+			finalAccessToken = new AccessToken(m.accesstoken(),m.secrettoken);
+
+			twitter.setOAuthAccessToken(finalAccessToken);
+			try {
+				// 계정명 획득
+				User user = twitter.verifyCredentials();
+				System.out.println(user.getScreenName());
+
+				// 계정에 트윗 등록
+				String msg = "properties 파일 테스트";
+				Status status = twitter.updateStatus(msg);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
 }
